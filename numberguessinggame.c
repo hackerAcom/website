f77e22152e6491dcb3903e9f42c7fa4c158af78a
hackerAcom <120191235+hackerAcom@users.noreply.github.com>
2022-12-23 19:57:22 +0545

    Update numberguessinggame.c

--- numberguessinggame.c
+++ numberguessinggame.c
@@ -2,42 +2,59 @@
 int main()
 {
     int luckynumber = 99; // declare luckynumber for win
-    int i;
+    int i; 
+    printf("you have three chance to guess luckynumber\n");
     printf("enter number between (1-100):"); // input number by user for guessing the number for win
-    printf("you have three chance to guess luckynumber");
+   
     scanf("%d", &luckynumber);
+    if (luckynumber>100)
+    {
+        printf("you choose greater than 100 and you are wrong\n");
+    }
+    
     if (luckynumber == 99) // condition for luckynumber
     {
         printf("congratulation your win");
     }
     else
     {
-        printf("your choose unlucky number \n");
-        printf("your have two chance remain:\n");
+        printf("you choose unlucky number \n");
+        printf("you have two chance remain:\n");
         for (i = 1; i < 2; i++)
         {
             printf("enter number between (1-100):");
             scanf("%d", &luckynumber);
+            if (luckynumber>100)
+            {
+            printf("you choose greater than 100 and you are wrong\n");
+            }
+            
         }
         if (luckynumber==99)
         {
              printf("congratulation your win");
         }
         else{
-            printf("your choose unlucky number\n");
-            printf("your have one chance remain:\n");
+            printf("you choose unlucky number\n");
+            printf("you have one chance remain:\n");
             for (i = 1; i < 2; i++)
         {
             printf("enter number between (1-100):");
             scanf("%d", &luckynumber);
+            if (luckynumber>100)
+            {
+                printf("you choose greater than 100 and you are wrong\n");
+            }
+            
         }
         if (luckynumber==99)
         {
-             printf("congratulation your win");
+             printf("congratulation your win \n");
+             printf("\nif you are  wrong in last chance then your \n are idiot but you are not idiot  ");
         }
         else{
-            printf("your choose unlucky number\n");
-            printf("your have no chance remain\n:");
+            printf("you choose unlucky number\n");
+            printf("you have no chance remain\n:");
         }
         
         }
